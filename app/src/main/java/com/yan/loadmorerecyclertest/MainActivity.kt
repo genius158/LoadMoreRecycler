163646ee4d59e92434c059a0a2307b04e71271eb
yan <yanxianwei1114@gmail.com>
2017-05-28 21:17:19 +0800

    recycler view with load more

--- app/src/main/java/com/yan/loadmorerecyclertest/MainActivity.kt
+++ app/src/main/java/com/yan/loadmorerecyclertest/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolBar)
 
         init()
     }
